3b8cc74ad19e5a8cf6c4e5ffebb379b278679b2d
Kachii <Kacheokcheong@gmail.com>
2020-12-03 16:44:16 +0000

    second commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './App.css';
+
+const App = () => (
+    <div className="App">
+      <h1>Kachii's React Page, it works!</h1>
+    </div>
+  );
+
+export default App;
